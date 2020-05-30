1598eebd01667c3dcbf3c3853325314f72a02468
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-29 23:26:00 -0500

    Vista CRUD con CBV DeleteView

--- pages/views.py
+++ pages/views.py
@@ -24,5 +24,9 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
+class PageDelete(DeleteView):
+    model = Page
+    success_url = reverse_lazy('pages:pages')
+
 
     
