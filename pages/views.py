9ed86d8a8b30a1c1b69bfb488cf20ecc7c7ed24a
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-30 01:00:49 -0500

    Creando un Mixin de identificacion

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,18 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+class StaffRequiredMixin(object):
+    """Este Mixin Requerirá que el usuario sea miembro del Staff"""
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
+    
 
 # Create your views here.
 class PageListView(ListView):
@@ -12,12 +22,14 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm  
     success_url = reverse_lazy('pages:pages')
 
-class PageUpdate(UpdateView):
+    
+
+class PageUpdate(StaffRequiredMixin, UpdateView):
     model = Page
     form_class = PageForm 
     template_name_suffix = '_update_form'
@@ -25,7 +37,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
-class PageDelete(DeleteView):
+class PageDelete(StaffRequiredMixin, DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
 
