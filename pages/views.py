41f9b6574af7485d55af1c7bc2d9f598b4397ff0
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-29 23:10:30 -0500

    Vista CRUD con CBV UpdateView

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page
 
@@ -15,5 +15,14 @@ class PageCreate(CreateView):
     model = Page
     fields = ['title', 'content', 'order']   
     success_url = reverse_lazy('pages:pages')
+
+class PageUpdate(UpdateView):
+    model = Page
+    fields = ['title', 'content', 'order']
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self):
+        return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+
 
     
