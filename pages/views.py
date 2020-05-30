40bd5f6881b50fc3b9fb02eb6310f04a27804bee
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-30 16:15:10 -0500

    Usando decoradores de identificacion

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,18 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
 
 class StaffRequiredMixin(object):
-    """Este Mixin Requerirá que el usuario sea miembro del Staff"""
+    """
+    Este Mixin Requerirá que el usuario sea miembro del Staff
+    """
+    @method_decorator(staff_member_required)
     def dispatch(self, request, *args, **kwargs):
         if not request.user.is_staff:
             return redirect(reverse_lazy('admin:login'))
@@ -22,14 +27,14 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-class PageCreate(StaffRequiredMixin, CreateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreate(CreateView):
     model = Page
     form_class = PageForm  
     success_url = reverse_lazy('pages:pages')
 
-    
-
-class PageUpdate(StaffRequiredMixin, UpdateView):
+@method_decorator(staff_member_required, name='dispatch')    
+class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm 
     template_name_suffix = '_update_form'
@@ -37,7 +42,8 @@ class PageUpdate(StaffRequiredMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
-class PageDelete(StaffRequiredMixin, DeleteView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
 
