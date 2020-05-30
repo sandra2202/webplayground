68d7aed8992393f3480a12aaee80f9f02115fc0e
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-30 00:19:25 -0500

    Formularios para Modelos en CBV

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
 from .models import Page
+from .forms import PageForm
 
 # Create your views here.
 class PageListView(ListView):
@@ -13,12 +14,12 @@ class PageDetailView(DetailView):
 
 class PageCreate(CreateView):
     model = Page
-    fields = ['title', 'content', 'order']   
+    form_class = PageForm  
     success_url = reverse_lazy('pages:pages')
 
 class PageUpdate(UpdateView):
     model = Page
-    fields = ['title', 'content', 'order']
+    form_class = PageForm 
     template_name_suffix = '_update_form'
 
     def get_success_url(self):
