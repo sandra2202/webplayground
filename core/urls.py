94b95f67ae33e18b221ac1778599395c43c3c59e
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-29 19:02:39 -0500

    Revision App Core CBV y TemplateView

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import HomePageView, SamplePageView
 
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('sample/', views.sample, name="sample"),
+    path('', HomePageView.as_view(), name="home"),
+    path('sample/', SamplePageView.as_view(), name="sample"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,15 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+class HomePageView(TemplateView):
+    template_name = "core/home.html"
+    def get_context_data(self, ):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Mi Super Web Playground"
+        return context
 
-def sample(request):
-    return render(request, "core/sample.html")
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {'title':"Mi Super Web Playground"})
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
