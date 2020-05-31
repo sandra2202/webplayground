1c43213223cee588d43080253f48802a6cc91f91
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-31 00:19:04 -0500

    Mejorando el formulario de perfil

--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from .forms import UserCreationFormWithEmail    
+from .forms import UserCreationFormWithEmail, ProfileForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
@@ -29,8 +29,7 @@ class SignUpView(CreateView):
 
 @method_decorator(login_required, name='dispatch')
 class ProfileUpdate(UpdateView):
-    model = Profile
-    fields = ['avatar', 'bio', 'link']
+    form_class = ProfileForm
     success_url = reverse_lazy('profile')
     template_name = 'registration/profile_form.html'
 
