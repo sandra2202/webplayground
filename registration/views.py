6ce8db80270a55b8a0f6a7b8f27d7025a92452a1
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-30 23:36:23 -0500

    Haciendo editable el perfil

--- registration/views.py
+++ registration/views.py
@@ -1,10 +1,11 @@
 from .forms import UserCreationFormWithEmail    
 from django.views.generic import CreateView
-from django.views.generic.base import TemplateView
+from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
 from django import forms
+from .models import Profile
 
 # Create your views here.
 class SignUpView(CreateView):
@@ -27,6 +28,14 @@ class SignUpView(CreateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class ProfileUpdate(TemplateView):
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = ['avatar', 'bio', 'link']
+    success_url = reverse_lazy('profile')
     template_name = 'registration/profile_form.html'
+
+    def get_object(self):
+        #recuperar el objeto que se va a editar
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        return profile
     
