c2f9c2295b7bfcf1af15918e361031125fde702c
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-31 18:22:16 -0500

    Solucion Novena App Profiles Perfiles Publicos

--- webplayground/settings.py
+++ webplayground/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor', 
     'core',
+    'profiles',
     'pages.apps.PagesConfig',
 ]
 
--- webplayground/urls.py
+++ webplayground/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.urls import pages_patterns
+from profiles.urls import profiles_patterns
 from django.conf import settings
+
 urlpatterns = [
 
     path('', include('core.urls')),
@@ -25,6 +27,9 @@ urlpatterns = [
     #Patch de Auth
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('registration.urls')),
+    # Paths de profiles
+    path('profiles/', include(profiles_patterns)),
+
 ]
 
 if settings.DEBUG:
