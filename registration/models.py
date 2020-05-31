20cd53d108570dbebbae126b79f66a63117b351b
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-31 17:10:10 -0500

    Introduccion a las senales

--- registration/models.py
+++ registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver 
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,3 +9,9 @@ class Profile(models.Model):
     avatar =models.ImageField(upload_to='profiles', null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(max_length=200, null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        Profile.objects.get_or_create(user=instance)
+        print("Se acaba de crear un usuario y su perfil enlazado")
