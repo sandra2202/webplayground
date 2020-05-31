58f65806b6ed460593b7b10a4a85a250223802d1
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-31 01:00:52 -0500

    Opcion de editar el email

--- registration/forms.py
+++ registration/forms.py
@@ -26,3 +26,19 @@ class ProfileForm(forms.ModelForm):
             'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'row':3, 'placeholder':'Biografia'}),
             'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
+
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como máximo y debe ser válido")
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado, prueba con otro.")
+        return email
+    
--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from .forms import UserCreationFormWithEmail, ProfileForm
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
@@ -38,3 +38,20 @@ class ProfileUpdate(UpdateView):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
     
+
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email_form.html'
+
+    def get_object(self):
+        #recuperar el objeto que se va a editar
+        return self.request.user
+
+    def get_form(self, form_class=None):        
+        form = super(EmailUpdate, self).get_form()
+        # Modificar en tiempo real 
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Email'})
+        return form
