898400567dc754369ed444a271ed1b4cb913a300
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-30 19:53:01 -0500

    Email Obligatorio al Registrarse

---
+++ registration/forms.py
@@ -0,0 +1,10 @@
+from django import forms 
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserCreationFormWithEmail(UserCreationForm):
+    email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como máximo y debe ser válido")
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail    
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
 
 # Create your views here.
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
@@ -16,6 +16,7 @@ class SignUpView(CreateView):
         form = super(SignUpView, self).get_form()
         # Modificar en tiempo real 
         form.fields['username'].widget = forms.TextInput(attrs={'class':'form-control mb-2', 'placeholder':'Nombre de usuario'})
+        form.fields['email'].widget = forms.EmailInput(attrs={'class':'form-control mb-2', 'placeholder':'Dirección de email'})
         form.fields['password1'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Contraseña'})
         form.fields['password2'].widget = forms.PasswordInput(attrs={'class':'form-control mb-2', 'placeholder':'Confirmar contraseña'})
         return form
