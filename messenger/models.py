06d9d16a6b9d7bb864a7b1877f549a6bf16157e2
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-31 23:33:47 -0500

    TDD 2 Refactorizacion

--- messenger/models.py
+++ messenger/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
 
 # Create your models here.
 class  Message(models.Model):
@@ -14,3 +15,23 @@ class  Message(models.Model):
 class Thread(models.Model):
     users = models.ManyToManyField(User, related_name='threads')
     messages = models.ManyToManyField(Message)
+
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop("instance", None)
+    action = kwargs.pop("action", None)
+    pk_set = kwargs.pop("pk_set", None)
+    print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action is "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print("Ups, ({}) no forma parte del hilo".format(msg.user))
+                false_pk_set.add(msg_pk)
+
+    #Buscar los mensajes de false_pk_set que están en pk_set y los borramos de pk_set
+    pk_set.difference_update(false_pk_set)
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
--- messenger/tests.py
+++ messenger/tests.py
@@ -7,6 +7,7 @@ class ThreadTestCase(TestCase):
     def setUp(self):
         self.user1 = User.objects.create_user('user1', None, 'test1234')
         self.user2 = User.objects.create_user('user2', None, 'test1234')
+        self.user3 = User.objects.create_user('user3', None, 'test1234')        
 
         self.thread = Thread.objects.create()
 
@@ -32,6 +33,15 @@ class ThreadTestCase(TestCase):
 
         for message in self.thread.messages.all():
             print("({}): {}".format(message.user, message.content))
+
+    def test_add_message_from_user_not_in_thread(self):
+        self.thread.users.add(self.user1, self.user2)
+        message1 = Message.objects.create(user=self.user1, content="Muy Buenas")
+        message2 = Message.objects.create(user=self.user2, content="Hola")
+        message3 = Message.objects.create(user=self.user3, content="Soy Sandra")
+        self.thread.messages.add(message1, message2, message3)
+        self.assertEqual(len(self.thread.messages.all()), 2)
+
 
 
 
