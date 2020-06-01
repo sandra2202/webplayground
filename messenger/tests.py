9eb43a72529183729547baefda8f6db584c59127
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-05-31 22:23:44 -0500

    TDD Primeras Pruebas

--- messenger/tests.py
+++ messenger/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Thread, Message
 
 # Create your tests here.
+class ThreadTestCase(TestCase):
+    def setUp(self):
+        self.user1 = User.objects.create_user('user1', None, 'test1234')
+        self.user2 = User.objects.create_user('user2', None, 'test1234')
+
+        self.thread = Thread.objects.create()
+
+    def test_add_users_to_thread(self):
+        self.thread.users.add(self.user1, self.user2)
+        self.assertEqual(len(self.thread.users.all()), 2)
+
+    def test_filter_thread_by_users(self):
+        self.thread.users.add(self.user1, self.user2)
+        threads = Thread.objects.filter(users=self.user1).filter(users=self.user2)
+        self.assertEqual(self.thread, threads[0])
+
+    def test_filter_non_existent_thread(self):
+        threads = Thread.objects.filter(users=self.user1).filter(users=self.user2)
+        self.assertEqual(len(threads), 0)
+
+    def test_add_messages_to_thread(self):
+        self.thread.users.add(self.user1, self.user2)
+        message1 = Message.objects.create(user=self.user1, content="Muy Buenas")
+        message2 = Message.objects.create(user=self.user2, content="Hola")
+        self.thread.messages.add(message1, message2)
+        self.assertEqual(len(self.thread.messages.all()), 2)
+
+        for message in self.thread.messages.all():
+            print("({}): {}".format(message.user, message.content))
+
+
+
+
--- registration/models.py
+++ registration/models.py
@@ -22,4 +22,4 @@ class Profile(models.Model):
 def ensure_profile_exists(sender, instance, **kwargs):
     if kwargs.get('created', False):
         Profile.objects.get_or_create(user=instance)
-        print("Se acaba de crear un usuario y su perfil enlazado")
+        #print("Se acaba de crear un usuario y su perfil enlazado")
