c06f653e9e80015e51125eea4878f70919f38fb1
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-01 23:39:22 -0500

    Mensajes asincronos con JS 2 Desarrollo

--- messenger/views.py
+++ messenger/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
-from .models import Thread
+from .models import Thread, Message
 from django.http import Http404, JsonResponse
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -24,7 +25,15 @@ class ThreadDetail(DetailView):
         return obj
 
 def add_message(request, pk):
-    print(request.GET)
     json_response = {'created':False}
+    if request.user.is_authenticated:
+        content = request.GET.get('content', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)
+            message = Message.objects.create(user=request.user, content=content)
+            thread.messages.add(message)
+            json_response['created'] = True
+    else:
+        raise Http404("User is not authenticated")    
     return JsonResponse(json_response)
 
