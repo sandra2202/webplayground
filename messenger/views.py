dbb126f2fa38501e680db4f0dbc51fc60e074211
Sandra Gutierrez <sandra2202gutierrez@gmail.com>
2020-06-01 22:08:25 -0500

    Mensajes asincronos con JS Primer Concepto

--- messenger/views.py
+++ messenger/views.py
@@ -3,7 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 from .models import Thread
-from django.http import Http404
+from django.http import Http404, JsonResponse
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -22,4 +22,9 @@ class ThreadDetail(DetailView):
         if self.request.user not in obj.users.all():
             raise Http404()
         return obj
+
+def add_message(request, pk):
+    print(request.GET)
+    json_response = {'created':False}
+    return JsonResponse(json_response)
 
